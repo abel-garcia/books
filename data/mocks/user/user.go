3df9e01da979d877a4800010655c3258874aa6f7
Abel Garcia <abel.garcia@mattel.com>
2022-06-27 03:36:58 -0500

    add test example

---
+++ data/mocks/user/user.go
@@ -0,0 +1,32 @@
+﻿package user
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	domain "github.com/wackGarcia/books/domain/user"
+)
+
+type Repository struct {
+}
+
+func NewUserMockRepository() *Repository {
+	return &Repository{}
+}
+
+func (store *Repository) Create(ctx context.Context, user *domain.User) error {
+	return errors.New("some error")
+}
+
+func (store *Repository) Get(ctx context.Context, username string) (*domain.User, error) {
+
+	return &domain.User{
+		ID:        uuid.NewString(),
+		UserName:  username,
+		Password:  "123456",
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+	}, nil
+}
---
+++ http/user_test.go
@@ -0,0 +1,69 @@
+﻿package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	mockRepository "github.com/wackGarcia/books/data/mocks/user"
+	"github.com/wackGarcia/books/domain/user"
+)
+
+func TestCreateHandler(t *testing.T) {
+
+	testCases := map[string]struct {
+		statusCode int
+		user       UserRequest
+	}{
+		"success": {
+			statusCode: http.StatusOK,
+			user: UserRequest{
+				UserName: "abelgarcia",
+				Password: "12345678",
+			},
+		},
+		// Fail password empty.
+		"badRequest": {
+			statusCode: http.StatusOK,
+			user: UserRequest{
+				UserName: "abelgarcia",
+			},
+		},
+	}
+
+	for testName, testCase := range testCases {
+		testName, testCase := testName, testCase
+		t.Run(testName, func(t *testing.T) {
+			userRepository := mockRepository.NewUserMockRepository()
+			userService := user.NewUserService(userRepository)
+
+			// Prepare body request.
+			requestBody, err := json.Marshal(map[string]interface{}{
+				"username": testCase.user.UserName,
+				"password": testCase.user.Password,
+			})
+			if err != nil {
+				t.Fatalf("Error marshaling body request %v", err)
+			}
+
+			req, err := http.NewRequest(http.MethodPost, "", bytes.NewBuffer(requestBody))
+			if err != nil {
+				t.Fatalf("error creating http request %v", err)
+			}
+
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+
+			ctx.Request = req
+			h := &Handler{User: userService}
+			h.UserCreateHandler()(ctx)
+
+			if status := w.Code; status != testCase.statusCode {
+				t.Errorf("unexpected HTTP response code, expected: %d, response: %d", testCase.statusCode, w.Code)
+			}
+		})
+	}
+}
